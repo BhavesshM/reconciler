a201fe04ac6b97a83ed73e96f991277ebc96a720
Sagar Singh <sagarsingh7744@gmail.com>
2025-01-07 15:58:09 +0530

    Adding envariable
    Adding variable for post production

--- reconciler/settings.py
+++ reconciler/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fj6r@-@)5h34xisoy^mc&!xf%@8(v9&g4d9_cflfh%q0c=)zuu"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
