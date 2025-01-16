ca5faaa9497ef668344f1cb10dcde51103a2e5e6
Bhavesh Mehta <bhavesh.mehta@loophealth.com>
2025-01-16 14:34:46 +0530

    added docker file and few other changes

--- reconciler/settings.py
+++ reconciler/settings.py
@@ -21,18 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY="django-insecure-fj6r@-@)5h34xisoy^mc&!xf%@8(v9&g4d9_cflfh%q0c=)zuu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG=False
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS=["*"]
+CSRF_TRUSTED_ORIGINS=["https://reconciler-ops-tool-509581839768.asia-south1.run.app/"] 
 
 # SECRET_KEY = "django-insecure-fj6r@-@)5h34xisoy^mc&!xf%@8(v9&g4d9_cflfh%q0c=)zuu"
 
 # DEBUG = True
 
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ["0.0.0.0"]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
